0d35b706f3f6a16cc9f207e812c66116daba034f
Damon Cichowski <damoncichowski@DOC.local>
2017-09-14 17:04:41 -0400

    Final Version

--- script.js
+++ script.js
@@ -2,16 +2,28 @@ window.onload = function(e){
     console.log("window.onload");
 
 
+    // var counter = 5;
+    // var interval = setInterval(function() {
+    // counter--;
+    // // Display 'counter' wherever you want to display it.
+    // if (counter < 1) {
+    //     // Display a login box
+    //     clearInterval(interval);
+    //   }
+    // }, 1000);
+    //
+    // setTimeout(function(){ alert("Hello"); }, 3000);
 
 
+    setTimeout(function(){
+      console.log("setTimeout");
+      $('#myModal').css('visibility', 'visible');
+    }, 3000);
 
+    $('#closeModalBtn').on('click', closeModal);
+    function closeModal() {
+      console.log("closeModal");
+      $('#myModal').css('visibility', 'hidden');
+    }
 
-
-
-
-
-
-
-
-    
 }
